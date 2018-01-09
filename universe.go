877c69489d564bbb4f6cf83d8952a36629c5ea39
Nathan Fisher <nfisher@junctionbox.ca>
2018-01-09 15:36:01 -0300

    Add lookups in JsonUniverse and LoadUniverse function.

--- universe.go
+++ universe.go
@@ -1,5 +1,11 @@
 package swapi
 
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
 type Universe struct {
 	Films     Films
 	People    People
@@ -9,6 +15,21 @@ type Universe struct {
 	Vehicles  Vehicles
 }
 
+func LoadUniverse(filename string) (*JsonUniverse, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var ju JsonUniverse
+
+	if err = json.NewDecoder(f).Decode(&ju); err != nil {
+		return nil, err
+	}
+
+	return &ju, nil
+}
+
 type JsonUniverse struct {
 	Films     map[string]Film
 	People    map[string]Person
@@ -16,6 +37,67 @@ type JsonUniverse struct {
 	Species   map[string]Specie
 	Starships map[string]Starship
 	Vehicles  map[string]Vehicle
+}
+
+var FilmNotFound = errors.New("Film not found")
+var PersonNotFound = errors.New("Person not found")
+var PlanetNotFound = errors.New("Planet not found. Death star nearby?")
+var SpecieNotFound = errors.New("Specie not found")
+var StarshipNotFound = errors.New("Starship not found")
+var VehicleNotFound = errors.New("Vehicle not found")
+
+func (ju *JsonUniverse) Film(id string) (*Film, error) {
+	f, ok := ju.Films[id]
+	if !ok {
+		return nil, FilmNotFound
+	}
+
+	return &f, nil
+}
+
+func (ju *JsonUniverse) Person(id string) (*Person, error) {
+	f, ok := ju.People[id]
+	if !ok {
+		return nil, PersonNotFound
+	}
+
+	return &f, nil
+}
+
+func (ju *JsonUniverse) Planet(id string) (*Planet, error) {
+	f, ok := ju.Planets[id]
+	if !ok {
+		return nil, SpecieNotFound
+	}
+
+	return &f, nil
+}
+
+func (ju *JsonUniverse) Specie(id string) (*Specie, error) {
+	f, ok := ju.Species[id]
+	if !ok {
+		return nil, SpecieNotFound
+	}
+
+	return &f, nil
+}
+
+func (ju *JsonUniverse) Starship(id string) (*Starship, error) {
+	f, ok := ju.Starships[id]
+	if !ok {
+		return nil, StarshipNotFound
+	}
+
+	return &f, nil
+}
+
+func (ju *JsonUniverse) Vehicle(id string) (*Vehicle, error) {
+	f, ok := ju.Vehicles[id]
+	if !ok {
+		return nil, VehicleNotFound
+	}
+
+	return &f, nil
 }
 
 func Struct2map(u *Universe) *JsonUniverse {
